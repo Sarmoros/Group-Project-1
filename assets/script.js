a22d7354735ab21e1192b531c0858673f1f3fbb1
rodrigoaoliva <rodrigoaoliva1@gmail.com>
2023-11-12 19:07:20 -0500

    changed HTML and JS

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,104 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const locationInput = document.getElementById("location");
-    const getWeatherButton = document.getElementById("get-weather");
+    const locationsInput = document.getElementById("locations");
+    const compareWeatherButton = document.getElementById("compare-weather");
     const weatherData = document.getElementById("weather-data");
-    const eventRecommendationText = document.getElementById("event-recommendation-text");
+    const imageContainer = document.getElementById("images");
+    let map;
 
-    getWeatherButton.addEventListener("click", function () {
-        const location = locationInput.value;
+    compareWeatherButton.addEventListener("click", function () {
+        const locations = locationsInput.value.split(",").map(location => location.trim());
 
-        const apiKey = '4e6e807ee59834126e8fdbcfad716167'; // Replace with your OpenWeatherMap API key
-        const weatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+        if (locations.length < 2) {
+            alert("Please enter at least two locations for comparison.");
+            return;
+        }
 
-        // Fetch weather data from OpenWeatherMap
-        fetch(weatherApiUrl)
-            .then(response => response.json())
-            .then(weatherData => {
-                if (weatherData.cod === 200) {
-                    const { name, main, weather } = weatherData;
-                    const { temp, humidity } = main;
-                    const { description } = weather[0];
+        if (locations.length >= 4) {
+            alert("Please enter 3 locations or less.");
+            return;
+        }
 
-                    // Display weather information
-                    weatherData.innerHTML = `
-                        <p>Location: ${name}</p>
-                        <p>Temperature: ${temp}°C</p>
-                        <p>Humidity: ${humidity}%</p>
-                        <p>Weather: ${description}</p>
+        // Clear previous data
+        weatherData.innerHTML = "";
+        imageContainer.innerHTML = "";
+
+        // Fetch weather data and images for each location
+        locations.forEach(location => {
+            const openWeatherMapApiKey = '4e6e807ee59834126e8fdbcfad716167';
+            const unsplashAccessKey = '2lV0OAt5aYO0BI3SaBX7whHE3sBxKNPAwXB1_3jdHbg'; // Replace with your actual Unsplash access key
+            const openWeatherMapUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${openWeatherMapApiKey}&units=metric`;
+            const unsplashUrl = `https://api.unsplash.com/photos/random?query=${location}&client_id=${unsplashAccessKey}`;
+
+            // Fetch weather data
+            fetch(openWeatherMapUrl)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.cod === 200) {
+                        const { name, main, weather } = data;
+                        const { temp, humidity } = main;
+                        const { description } = weather[0];
+
+                        // Display weather data
+                        weatherData.innerHTML += `
+                            <div>
+                                <h3>${name}</h3>
+                                <p>Temperature: ${temp}°C</p>
+                                <p>Humidity: ${humidity}%</p>
+                                <p>Weather: ${description}</p>
+                            </div>
+                        `;
+
+                        // Display the map for the event location
+                        showMap(data.coord.lat, data.coord.lon, name);
+                    } else {
+                        weatherData.innerHTML += `<p>${location}: Location not found</p>`;
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+            // Fetch images from Unsplash
+            fetch(unsplashUrl)
+                .then(response => response.json())
+                .then(imageData => {
+                    const imageUrl = imageData.urls.regular;
+
+                    // Display image
+                    imageContainer.innerHTML += `
+                        <div>
+                            <h3>${location}</h3>
+                            <img src="${imageUrl}" alt="${location}">
+                        </div>
                     `;
-
-                    // Generate event recommendations based on weather and location
-                    generateEventRecommendations(location, description);
-                } else {
-                    weatherData.innerHTML = "Location not found";
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        });
     });
 
-    function generateEventRecommendations(location, weatherDescription) {
-        // Create a request to ChatGPT for event recommendations
-        const chatGptApiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
-
-        // Use a specific prompt for event recommendations based on location and weather
-        const prompt = `Suggest outdoor events in ${location} based on the weather "${weatherDescription}".`;
-
-        const requestBody = {
-            prompt,
-            max_tokens: 100,
-        };
-
-        const headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer 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', // Replace with your ChatGPT API key
-        };
-
-        // Make a POST request to ChatGPT API
-        fetch(chatGptApiUrl, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(requestBody),
-        })
-        .then(response => {
-            if (response.status === 200) {
-                return response.json();
-            } else {
-                throw new Error('ChatGPT API request failed');
-            }
-        })
-        .then(responseData => {
-            // Display the event recommendation from ChatGPT
-            const recommendation = responseData.choices[0].text;
-            eventRecommendationText.textContent = recommendation;
-        })
-        .catch(error => {
-            console.error(error);
+    // Function to initialize the map
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 0, lng: 0 },
+            zoom: 2
         });
     }
+
+    // Function to show the map for the event location
+    function showMap(latitude, longitude, locationName) {
+        const eventLocation = new google.maps.LatLng(latitude, longitude);
+        map.setCenter(eventLocation);
+        map.setZoom(2); // You can adjust the zoom level as needed
+
+        const marker = new google.maps.Marker({
+            position: eventLocation,
+            map: map,
+            title: locationName
+        });
+    }
+
+    // Ensure that the map is initialized after the Google Maps API is fully loaded
+    google.maps.event.addDomListener(window, 'load', initMap);
 });
